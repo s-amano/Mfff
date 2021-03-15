5778e7b2132be6a84d0acbb9b4da552b40c3be59
{s-amano} <{amahaya0831@gmail.com}>
2021-03-15 23:11:10 +0900

    プロフィールアイアイコンからプロフィール詳細へ

--- src/frontend/src/components/ProfileIcon.js
+++ src/frontend/src/components/ProfileIcon.js
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Avatar from '@material-ui/core/Avatar';
+import { ApiContext } from '../context/ApiContext';
+import { Button } from '@material-ui/core';
 
 const ProfileIcon = ({ userImg }) => {
-  const CheckProfileInfo = () => (event) => {
-    window.location.href = '/profile-info';
+  const { user, profile, setSelectedProfile, setSelectedUser } = useContext(ApiContext);
+  const CheckUserProfile = (userId, profId) => {
+    setSelectedProfile(profId);
+    setSelectedUser(userId);
   };
   return (
-    <button className="Icon" onClick={CheckProfileInfo()}>
-      {userImg ? (
-        <Avatar alt="profile icon" src={userImg} />
-      ) : (
-        <Avatar alt="profile icon" src="http://127.0.0.1:8000/media/image/null.png" />
-      )}
-    </button>
+    // <button className="Icon" onClick={CheckProfileInfo()}>
+    //   {userImg ? (
+    //     <Avatar alt="profile icon" src={userImg} />
+    //   ) : (
+    //     <Avatar alt="profile icon" src="http://127.0.0.1:8000/media/image/null.png" />
+    //   )}
+    // </button>
+
+    <Button size="small" className="Icon" onClick={() => CheckUserProfile(user.id, profile.id)} style={{ flex: 1 }}>
+      <Avatar alt="profile icon" src={userImg} />
+    </Button>
   );
 };
 
