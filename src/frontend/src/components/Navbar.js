36604213fa9fbf69fe8563a181ec0ce51fbf96b2
{s-amano} <{amahaya0831@gmail.com}>
2021-03-18 01:02:02 +0900

    友達申請

--- src/frontend/src/components/Navbar.js
+++ src/frontend/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { ApiContext } from '../context/ApiContext';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
@@ -8,16 +8,35 @@ import { FiLogOut } from 'react-icons/fi';
 import { withCookies } from 'react-cookie';
 import LogoutButton from './LogoutButton';
 import ProfileIcon from './ProfileIcon';
+import AskModal from './AskModal';
 import { Link } from 'react-router-dom';
+import Badge from '@material-ui/core/Badge';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import Modal from 'react-modal';
 
 const useStyles = makeStyles((theme) => ({
   title: {
     flexGrow: 1,
   },
+  bg: {
+    marginRight: theme.spacing(1),
+  },
 }));
 
 const Navbar = (props) => {
-  const { user } = useContext(ApiContext);
+  // Modal.setAppElement("#root");
+  const { user, profile, profiles, users, askList, changeApprovalRequest } = useContext(ApiContext);
+
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const customStyles = {
+    content: {
+      top: '50%',
+      left: '42%',
+      right: 'auto',
+      bottom: 'auto',
+    },
+  };
 
   const classes = useStyles();
   return (
@@ -29,10 +48,43 @@ const Navbar = (props) => {
           </Link>
         </Typography>
 
-        <LogoutButton />
         <Link to="/profile-info" style={{ textDecoration: 'none', color: 'white' }}>
           <ProfileIcon userImg={user.img} />
         </Link>
+
+        <Badge
+          className={classes.bg}
+          badgeContent={
+            askList.filter((ask) => {
+              return (
+                ask.approved === false &&
+                profiles.filter((item) => {
+                  return item.userPro === ask.askFrom;
+                })[0]
+              );
+            }).length
+          }
+          color="secondary"
+        >
+          <NotificationsIcon onClick={() => setModalIsOpen(true)} />
+        </Badge>
+
+        <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)} style={customStyles}>
+          <Typography>友達申請</Typography>
+          <ul>
+            {profile.id &&
+              askList.map((ask) => (
+                <AskModal
+                  key={ask.id}
+                  ask={ask}
+                  askFromUser={users.filter((item) => {
+                    return item.id === ask.askFrom;
+                  })}
+                />
+              ))}
+          </ul>
+        </Modal>
+        <LogoutButton />
       </Toolbar>
     </AppBar>
   );
