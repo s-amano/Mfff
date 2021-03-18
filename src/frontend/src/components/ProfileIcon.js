9124abf31a1477ae783b44a76881d36e5ac01352
{s-amano} <{amahaya0831@gmail.com}>
2021-03-19 01:57:48 +0900

    ユーザー情報を修正可能に

--- src/frontend/src/components/ProfileIcon.js
+++ src/frontend/src/components/ProfileIcon.js
@@ -4,13 +4,15 @@ import { ApiContext } from '../context/ApiContext';
 import { Button } from '@material-ui/core';
 
 const ProfileIcon = ({ userImg }) => {
-  const { user, profile, setSelectedProfile, setSelectedUser } = useContext(ApiContext);
-  const CheckUserProfile = (userId, profId) => {
-    setSelectedProfile(profId);
-    setSelectedUser(userId);
-  };
+  const { user, profile, setSelectedProfile, setSelectedUser, getSpecificUserProfileInfo } = useContext(ApiContext);
+
   return (
-    <Button size="small" className="Icon" onClick={() => CheckUserProfile(user.id, profile.id)} style={{ flex: 1 }}>
+    <Button
+      size="small"
+      className="Icon"
+      onClick={() => getSpecificUserProfileInfo(user.id, profile.id)}
+      style={{ flex: 1 }}
+    >
       <Avatar alt="profile icon" src={userImg} />
     </Button>
   );
